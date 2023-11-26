59368d604d749b2a0e86419d13c5e5d81df8b7b6
kkanthan77 <151114338+kkanthan77@users.noreply.github.com>
2023-11-26 01:44:11 -0500

    Create append_values_to_list.tcl

---
+++ append_values_to_list.tcl
@@ -0,0 +1,8 @@
+#Appending values to list
+#Finding length of the list
+
+set fruits orange
+append fruits " " "banana"
+lappend fruits "redapple" 
+lappend fruits "pineapple"
+puts "length is [llength $fruits]"
