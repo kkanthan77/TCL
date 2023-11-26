1db902e904dd07927daee9ae88c4a0e58bcb003f
kkanthan77 <151114338+kkanthan77@users.noreply.github.com>
2023-11-26 01:54:35 -0500

    Create set_a_value_in_an_index.tcl

---
+++ set_a_value_in_an_index.tcl
@@ -0,0 +1,9 @@
+# Setting a value in a particular index of an existing list
+
+set colors {red blue green white}
+
+# lset sets the value of king in 0th index of the colors list
+
+lset colors 0 king
+
+puts $colors
